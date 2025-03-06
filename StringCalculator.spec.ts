035f6eea15c1e8c8ec4eba7adb059d8dd54f6498
Narayanan <narayanan.cs.38@gmail.com>
2025-03-06 19:34:27 +0530

    2: introduce the add method to StringCalculator class

--- StringCalculator.spec.ts
+++ StringCalculator.spec.ts
@@ -1,15 +1,25 @@
-
+import {StringCalculator} from './StringCalculator'
 
 describe("String Calculator test suite",()=>{
-
+    let calculator: StringCalculator
 beforeAll(()=>{
-
+   calculator = new StringCalculator()
 })
 
 test("nothing to test",()=>{
 
     expect("").toEqual("")
 })
+
+test("calculator should have add method",()=>{
+
+    expect(calculator.add).toBeDefined();
+    expect(typeof calculator.add).toBe('function')
+        
+    })
+    
+
+
 
 
 afterAll(()=>{
---
+++ StringCalculator.ts
@@ -0,0 +1,7 @@
+export class StringCalculator {
+  
+    add(numbers: string): number {
+      return 0
+    }
+  }
+  
