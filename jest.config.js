cb6363923411529c68763b8551a1bc082f17a0c1
Narayanan <narayanan.cs.38@gmail.com>
2025-03-11 21:00:10 +0530

    add jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    preset: 'ts-jest', // Tells Jest to use ts-jest for TypeScript files
+    testEnvironment: 'node', // or 'jsdom' if you're testing browser-specific code
+  };
+  
